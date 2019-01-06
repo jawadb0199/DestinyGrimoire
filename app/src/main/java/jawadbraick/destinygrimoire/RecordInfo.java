7865decc53451f45f70116167826d8d53005b05f
Jawad Braick <jawadb0199@gmail.co>
2019-01-06 15:00:51 -0500

    Added Constructor and Getters for RecordInfo class

--- app/src/main/java/jawadbraick/destinygrimoire/RecordInfo.java
+++ app/src/main/java/jawadbraick/destinygrimoire/RecordInfo.java
@@ -1,7 +1,19 @@
 package jawadbraick.destinygrimoire;
 
 public class RecordInfo{
-    int iconId;
-    String name;
-//  String recordName;
+    private int iconId;
+    private String name;
+
+    public RecordInfo(int iconId, String name){
+        this.iconId = iconId;
+        this.name = name;
+    }
+
+    public int getIconId(){
+        return iconId;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
