4449f6e3377d3e61a70e9f44e55b0779e0fe8b1d
Jawad Braick <jawadb0199@gmail.co>
2019-01-07 14:13:46 -0500

    Made database class into Singleton

--- app/src/main/java/jawadbraick/destinygrimoire/ManifestDatabase.java
+++ app/src/main/java/jawadbraick/destinygrimoire/ManifestDatabase.java
@@ -14,14 +14,16 @@ public abstract class ManifestDatabase extends RoomDatabase{
 
     public static ManifestDatabase getInstance(Context context){
         if (instance == null){
-            synchronized (ManifestDatabase.class) {
-                if (instance == null) {
-                    instance = RoomAsset.databaseBuilder(context, ManifestDatabase.class, "destiny2_manifest_lore.db").allowMainThreadQueries().build();
-                }
-            }
+            initDatabase(context);
         }
         return instance;
     }
-    private static void initDatabase(){}
+    private static void initDatabase(Context context){
+        synchronized (ManifestDatabase.class) {
+            if (instance == null) {
+                instance = RoomAsset.databaseBuilder(context, ManifestDatabase.class, "destiny2_manifest_lore.db").allowMainThreadQueries().build();
+            }
+        }
+    }
     public abstract DAO getDao();
 }
