715245874a7f640e514c0e0f26414b6a54130cd0
Jawad Braick <jawadb0199@gmail.co>
2019-01-06 20:32:11 -0500

    Added RecordDefinition Table to Database Schema

--- app/src/main/java/jawadbraick/destinygrimoire/ManifestDatabase.java
+++ app/src/main/java/jawadbraick/destinygrimoire/ManifestDatabase.java
@@ -4,7 +4,7 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
 
-@Database(entities = {PresentationNodeDefinition.class}, version = 2)
+@Database(entities = {PresentationNodeDefinition.class, RecordDefinition.class }, version = 2)
 @TypeConverters({Converters.class})
 public abstract class ManifestDatabase extends RoomDatabase{
     public abstract DAO getDao();
