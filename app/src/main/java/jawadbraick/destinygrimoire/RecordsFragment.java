c51c083e8a27360cc9b7a834c799e6e667928ec5
Jawad Braick <jawadb0199@gmail.co>
2019-01-09 13:22:57 -0500

    RecordsFragment now full screen and shows book name. (Empty RecyclerView)

--- app/src/main/java/jawadbraick/destinygrimoire/RecordsFragment.java
+++ app/src/main/java/jawadbraick/destinygrimoire/RecordsFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class RecordsFragment extends Fragment{
     String name;
@@ -14,7 +15,11 @@ public class RecordsFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_records, container, false);
+        View view = inflater.inflate(R.layout.fragment_records, container, false);
+        TextView header = view.findViewById(R.id.bookNameText);
+        header.setText(name);
+
+        return view;
     }
 
     public void setName(String name){
