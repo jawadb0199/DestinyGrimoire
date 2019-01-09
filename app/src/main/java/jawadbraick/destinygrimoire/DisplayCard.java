d10b4893726a548e1c75645b1cb1372d82d8115a
Jawad Braick <jawadb0199@gmail.co>
2019-01-09 15:02:05 -0500

    Optimized Imports

--- app/src/main/java/jawadbraick/destinygrimoire/DisplayCard.java
+++ app/src/main/java/jawadbraick/destinygrimoire/DisplayCard.java
@@ -2,23 +2,14 @@ package jawadbraick.destinygrimoire;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Typeface;
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.text.style.TypefaceSpan;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import java.lang.reflect.Type;
-import java.util.Set;
-
-//import uk.co.deanwild.flowtextview.FlowTextView;
 
 public class DisplayCard extends AppCompatActivity{
     private Context context;
