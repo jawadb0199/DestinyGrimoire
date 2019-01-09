f8519267fd7f45e5d4cc7ba564898a3155daf180
Jawad Braick <jawadb0199@gmail.co>
2019-01-09 14:56:20 -0500

    Added book icon to RecordsFragment header

--- app/src/main/java/jawadbraick/destinygrimoire/RecordsFragment.java
+++ app/src/main/java/jawadbraick/destinygrimoire/RecordsFragment.java
@@ -1,11 +1,13 @@
 package jawadbraick.destinygrimoire;
 
 import android.app.Fragment;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RecordsFragment extends Fragment{
@@ -16,7 +18,11 @@ public class RecordsFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_records, container, false);
-        TextView header = view.findViewById(R.id.bookNameText);
+
+        ImageView icon = (ImageView) view.findViewById(R.id.bookIcon);
+        icon.setImageResource(getImageResource(name));
+
+        TextView header = (TextView) view.findViewById(R.id.bookNameText);
         header.setText(name);
 
         return view;
@@ -28,5 +34,10 @@ public class RecordsFragment extends Fragment{
 
     public void setRecordIds(long[] recordIds){
         this.recordIds = recordIds;
+    }
+
+    private int getImageResource(String name){
+        String fileName = name.replaceAll("[^a-zA-Z0-9 ]", "").replaceAll(" ", "_").toLowerCase();
+        return getResources().getIdentifier(fileName, "drawable", getActivity().getPackageName());
     }
 }
