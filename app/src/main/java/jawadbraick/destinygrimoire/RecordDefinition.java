2017c82cb8d15e8c16c13d17b2886800ae32dd3b
Jawad Braick <jawadb0199@gmail.co>
2019-02-02 17:08:56 -0500

    Optimized Imports

--- app/src/main/java/jawadbraick/destinygrimoire/RecordDefinition.java
+++ app/src/main/java/jawadbraick/destinygrimoire/RecordDefinition.java
@@ -1,10 +1,6 @@
 package jawadbraick.destinygrimoire;
 
-import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
-
-import com.google.gson.JsonObject;
 
 @Entity(tableName = "DestinyRecordDefinition")
 public class RecordDefinition extends ManifestDefintion{}
