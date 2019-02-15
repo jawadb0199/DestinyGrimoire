eba2a416197e7307f01b324d773605e3a2858b92
Jawad Braick <jawadb0199@gmail.co>
2019-02-15 15:11:27 -0500

    Error Handling

--- app/src/main/java/jawadbraick/destinygrimoire/DisplayCard.java
+++ app/src/main/java/jawadbraick/destinygrimoire/DisplayCard.java
@@ -1,6 +1,5 @@
 package jawadbraick.destinygrimoire;
 
-import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,61 +7,63 @@ import android.text.Html;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.JsonObject;
 
 public class DisplayCard extends AppCompatActivity{
-    private Context context;
-    private static String intro = "<b> Abase yourself, weapons and instruments<br/>Submit yourself, shapes and gliders, automata all —<br/>I am Oryx, Lord of Shapes, Carver of Tablets<br/><br/>Behold my performance of the Last True Shape<br/>The final axiom<br/>Witness the space that I define<br/><br/>I approach the asymptote.<br/>I grow vast across topologies.<br/>I am not simply connected.</b>";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_card);
-        context = this;
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_display_card);
 
-        JsonObject cardJson = GrimoireContainer.getObject().getCardJson();
+            JsonObject cardJson = GrimoireContainer.getObject().getCardJson();
 
 //        Set cardImage source and Description Border (Background)
-        int cardImageFile = getImage(cardJson.get("cardId").toString());
-        ImageView cardImage = findViewById(R.id.cardImage);
-        cardImage.setImageResource(cardImageFile);
-        ImageView borderBackground = findViewById(R.id.background);
-        borderBackground.setImageResource(cardImageFile);
+            int cardImageFile = getImage(cardJson.get("cardId").toString());
+            ImageView cardImage = findViewById(R.id.cardImage);
+            cardImage.setImageResource(cardImageFile);
+            ImageView borderBackground = findViewById(R.id.background);
+            borderBackground.setImageResource(cardImageFile);
 
 //        Set Card Name Text
-        String cardName = cardJson.get("cardName").toString();
-        cardName = cardName.substring(1, cardName.length()-1);
-        TextView cardNameText = findViewById(R.id.cardName);
-        cardNameText.setText(cardName);
+            String cardName = cardJson.get("cardName").toString();
+            cardName = cardName.substring(1, cardName.length() - 1);
+            TextView cardNameText = findViewById(R.id.cardName);
+            cardNameText.setText(cardName);
 
 
 //        Set Card Intro Text and Attribution (if applicable)
-        if (cardJson.get("cardIntro") != null){
-            String cardIntro = stripHtml(cardJson.get("cardIntro").toString());
+            if (cardJson.get("cardIntro") != null) {
+                String cardIntro = stripHtml(cardJson.get("cardIntro").toString());
 //            String cardIntro = stripHtml(intro);
-            cardIntro = cardIntro.substring(1, cardIntro.length()-1);
-            TextView cardIntroText = findViewById(R.id.cardIntro);
-            cardIntroText.setText(cardIntro);
-            cardIntroText.setVisibility(View.VISIBLE);
-        }
-        if (cardJson.get("cardIntroAttribution") != null){
-            String cardIntroAttribution = stripHtml(cardJson.get("cardIntroAttribution").toString());
-            cardIntroAttribution = cardIntroAttribution.substring(1, cardIntroAttribution.length()-1);
-            TextView cardIntroAttributionText = findViewById(R.id.cardIntroAttribution);
-            cardIntroAttributionText.setText(cardIntroAttribution);
-            cardIntroAttributionText.setVisibility(View.VISIBLE);
+                cardIntro = cardIntro.substring(1, cardIntro.length() - 1);
+                TextView cardIntroText = findViewById(R.id.cardIntro);
+                cardIntroText.setText(cardIntro);
+                cardIntroText.setVisibility(View.VISIBLE);
+            }
+            if (cardJson.get("cardIntroAttribution") != null) {
+                String cardIntroAttribution = stripHtml(cardJson.get("cardIntroAttribution").toString());
+                cardIntroAttribution = cardIntroAttribution.substring(1, cardIntroAttribution.length() - 1);
+                TextView cardIntroAttributionText = findViewById(R.id.cardIntroAttribution);
+                cardIntroAttributionText.setText(cardIntroAttribution);
+                cardIntroAttributionText.setVisibility(View.VISIBLE);
 
-        }
+            }
 
 //        Set Card Description Text
-        if(cardJson.get("cardDescription") != null) {
-            String cardDescription = stripHtml(cardJson.get("cardDescription").toString());
-            cardDescription = cardDescription.substring(1, cardDescription.length() - 1);
-            TextView cardDescriptionText = findViewById(R.id.cardDescription);
-            cardDescriptionText.setText(cardDescription);
-        } else {
-            findViewById(R.id.cardDescriptionBlock).setVisibility(View.GONE);
+            if (cardJson.get("cardDescription") != null) {
+                String cardDescription = stripHtml(cardJson.get("cardDescription").toString());
+                cardDescription = cardDescription.substring(1, cardDescription.length() - 1);
+                TextView cardDescriptionText = findViewById(R.id.cardDescription);
+                cardDescriptionText.setText(cardDescription);
+            } else {
+                findViewById(R.id.cardDescriptionBlock).setVisibility(View.GONE);
+            }
+        } catch (Exception e){
+            Toast.makeText(this, "Error Loading Grimoire Card", Toast.LENGTH_LONG).show();
         }
 
     }
@@ -75,10 +76,10 @@ public class DisplayCard extends AppCompatActivity{
         }
     }
     private int getImage(String fileName){
-        Resources resources = context.getResources();
+        Resources resources = getResources();
         if (Character.isDigit(fileName.charAt(0))){
             fileName = 'c' + fileName;
         }
-        return resources.getIdentifier(fileName, "drawable", context.getPackageName());
+        return resources.getIdentifier(fileName, "drawable", getPackageName());
     }
 }
--- app/src/main/java/jawadbraick/destinygrimoire/Pages.java
+++ app/src/main/java/jawadbraick/destinygrimoire/Pages.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -15,47 +16,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pages extends AppCompatActivity{
-    List<String> adapterList;
-    GrimoireContainer grimoire = GrimoireContainer.getObject();
+    private List<String> adapterList;
+    private GrimoireContainer grimoire = GrimoireContainer.getObject();
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pages);
-
-        Thread t = new Thread(new Runnable(){
-            @Override
-            public void run(){
-                adapterList = createList(GrimoireContainer.getObject().getPageCollection());
-            }
-        });
-        t.start();
-
-        HeaderGridView gridView = findViewById(R.id.gridview);
-
-        LinearLayout header = (LinearLayout) getLayoutInflater().inflate(R.layout.pages_header, null);
-
-        TextView label = (TextView) header.getChildAt(1);
-        String theme = getIntent().getStringExtra("themeTag");
-        theme = theme.substring(0,1).toUpperCase() + theme.substring(1);
-        label.setText(theme);
-
-        TextView scoreText = (TextView) header.findViewById(R.id.score);
-        String score = getSharedPreferences("userData", Context.MODE_PRIVATE).getString("score", "");
-        scoreText.setText(score);
-
-        gridView.addHeaderView(header);
         try {
-            t.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            Toast.makeText(this, "Error Loading Grimoire Pages", Toast.LENGTH_LONG).show();
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_pages);
+
+            Thread t = new Thread(new Runnable(){
+                @Override
+                public void run(){
+                    adapterList = createList();
+                }
+            });
+            t.start();
+
+            HeaderGridView gridView = findViewById(R.id.gridview);
+
+            LinearLayout header = (LinearLayout) getLayoutInflater().inflate(R.layout.pages_header, null);
+
+            TextView label = (TextView) header.getChildAt(1);
+            String theme = getIntent().getStringExtra("themeTag");
+            theme = theme.substring(0, 1).toUpperCase() + theme.substring(1);
+            label.setText(theme);
+
+            TextView scoreText = (TextView) header.findViewById(R.id.score);
+            String score = getSharedPreferences("userData", Context.MODE_PRIVATE).getString("score", "");
+            scoreText.setText(score);
+
+            gridView.addHeaderView(header);
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            PagesAdapter pagesAdapter = new PagesAdapter(this, adapterList);
+            gridView.setAdapter(pagesAdapter);
+        } catch (Exception e){
+            Toast.makeText(this, "Error Loading Grimoire Pages", Toast.LENGTH_LONG).show();
         }
-        PagesAdapter pagesAdapter = new PagesAdapter(this, adapterList);
-        gridView.setAdapter(pagesAdapter);
 
     }
 
-    private List<String> createList(JsonArray pageCollection){
+    private List<String> createList(){
+        JsonArray pageCollection = grimoire.getPageCollection();
         ArrayList<String> pageNameList = new ArrayList<>();
 
         for(int i = 0; i < pageCollection.size(); i++){
