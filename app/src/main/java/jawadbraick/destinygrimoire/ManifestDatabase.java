4c81f97a668c66cac24538c746846cf7a8c9e4c9
Jawad Braick <jawadb0199@gmail.co>
2019-03-09 18:15:14 -0500

    Version 5 (1.4) - Fixed database not updating properly

--- app/src/main/java/jawadbraick/destinygrimoire/ManifestDatabase.java
+++ app/src/main/java/jawadbraick/destinygrimoire/ManifestDatabase.java
@@ -4,6 +4,8 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import com.huma.room_for_asset.RoomAsset;
 
@@ -21,6 +23,16 @@ public abstract class ManifestDatabase extends RoomDatabase{
     private static void initDatabase(Context context){
         synchronized (ManifestDatabase.class) {
             if (instance == null) {
+                int appVersion = PreferenceManager.getDefaultSharedPreferences(context).getInt("appVersion", -1);
+
+                if (appVersion != BuildConfig.VERSION_CODE){
+                    SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+                    editor.putInt("appVersion", BuildConfig.VERSION_CODE);
+                    editor.apply();
+                    editor.putBoolean("instantiated", false);
+                    editor.commit();
+                    context.deleteDatabase("destiny2_manifest_lore.db");
+                }
                 instance = RoomAsset.databaseBuilder(context, ManifestDatabase.class, "destiny2_manifest_lore.db").build();
             }
         }
